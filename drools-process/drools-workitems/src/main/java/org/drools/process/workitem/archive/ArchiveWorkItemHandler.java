8240cffcceee2308a4786c0bef55b22cdabea355
mark.proctor@jboss.com <mark.proctor@jboss.com@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2010-01-25 21:51:54 +0000

    -updated Archive work item to latest commons-archive
    git-svn-id: http://anonsvn.jboss.org/repos/labs/labs/jbossrules/trunk@31206 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-process/drools-workitems/src/main/java/org/drools/process/workitem/archive/ArchiveWorkItemHandler.java
+++ drools-process/drools-workitems/src/main/java/org/drools/process/workitem/archive/ArchiveWorkItemHandler.java
@@ -1,10 +1,17 @@
 package org.drools.process.workitem.archive;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
-import org.apache.commons.compress.Archive;
-import org.apache.commons.compress.ArchiverFactory;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.compressors.CompressorOutputStream;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
+import org.apache.commons.compress.utils.IOUtils;
 import org.drools.process.instance.WorkItemHandler;
 import org.drools.runtime.process.WorkItem;
 import org.drools.runtime.process.WorkItemManager;
@@ -14,14 +21,25 @@ public class ArchiveWorkItemHandler implements WorkItemHandler {
 	public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
 		String archive = (String) workItem.getParameter("Archive");
 		List<File> files = (List<File>) workItem.getParameter("Files");
+		
 		try {
-			Archive archiver = ArchiverFactory.getInstance("tar");
+	        OutputStream outputStream = new FileOutputStream( new File( archive) );	        
+	        ArchiveOutputStream os = new ArchiveStreamFactory().createArchiveOutputStream("tar", outputStream);
+	        
 			if (files != null) {
 				for (File file: files) {
-					archiver.add(file);
+			        final TarArchiveEntry entry = new TarArchiveEntry("testdata/test1.xml");
+			        entry.setModTime(0);
+			        entry.setSize(file.length());
+			        entry.setUserId(0);
+			        entry.setGroupId(0);
+			        entry.setMode(0100000);
+			        os.putArchiveEntry(entry);
+			        IOUtils.copy(new FileInputStream(file), os);
 				}
 			}
-			archiver.save(new File(archive));
+	        os.closeArchiveEntry();
+	        os.close();
 			manager.completeWorkItem(workItem.getId(), null);
 		} catch (Throwable t) {
 			t.printStackTrace();
