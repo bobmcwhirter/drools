8381fc5bfa0a4544d9605529f9f5e7d67c97c2d5
mark.proctor@jboss.com <mark.proctor@jboss.com@c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70>
2010-02-28 05:08:26 +0000

    JBRULES-2351 OSGi Ready -BPMN2 and DecisionTables providers are now registered properly -ServiceRegistry now does not add defaults to the main registry, instead it uses those as a fallback if nothing is registered.
    git-svn-id: http://anonsvn.jboss.org/repos/labs/labs/jbossrules/trunk@31866 c60d74c8-e8f6-0310-9e8f-d4a2fc68ab70

--- drools-process/drools-bpmn2/src/main/java/org/drools/osgi/bpmn2/Activator.java
+++ drools-process/drools-bpmn2/src/main/java/org/drools/osgi/bpmn2/Activator.java
@@ -2,6 +2,7 @@ package org.drools.osgi.bpmn2;
 
 import java.util.Hashtable;
 
+import org.drools.Service;
 import org.drools.bpmn2.BPMN2ProcessProviderImpl;
 import org.drools.compiler.BPMN2ProcessProvider;
 import org.osgi.framework.BundleActivator;
@@ -14,14 +15,16 @@ public class Activator
     private ServiceRegistration bpmn2ProcessReg;
 
     public void start(BundleContext bc) throws Exception {
-    	System.out.println( "registering BPMN2 process drools services" );
-        this.bpmn2ProcessReg = bc.registerService( BPMN2ProcessProvider.class.getName(),
-                                               new BPMN2ProcessProviderImpl(),
-                                               new Hashtable() );
+        System.out.println( "registering BPMN2 process drools services" );
+        this.bpmn2ProcessReg = bc.registerService( new String[]{ BPMN2ProcessProvider.class.getName(), Service.class.getName()},
+                                                   new BPMN2ProcessProviderImpl(),
+                                                   new Hashtable() );
+
         System.out.println( "drools BPMN2 process services registered" );
     }
 
     public void stop(BundleContext bc) throws Exception {
         this.bpmn2ProcessReg.unregister();
     }
+
 }
